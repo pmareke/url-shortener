3d8e314025007412cc2d5a3dc45dfefa8a728ad5
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2023-10-11 09:32:41 +0200

    feat: Refactor shortener.rb

--- app/services/shortener.rb
+++ app/services/shortener.rb
@@ -3,8 +3,7 @@ require 'digest'
 class Shortener
 
   def self.short input
-    short_url = Digest::SHA2.hexdigest input
-    short_url[0...32]
+    Digest::SHA2.hexdigest(input)[0...32]
   end
 
 end
